51b9d2547fa3981a5161eb4bcc9cf95d81c6f93c
MJ <mj838@outlook.com>
2016-03-28 17:28:16 +0100

    Initial commit - default Visual Studio ASP.NET MVC 5 project with updated nuget packages

---
+++ StaffPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StaffPortal.Startup))]
+namespace StaffPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
