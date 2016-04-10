815fc90bf94eb2b0cfde992eb8b6b231b330d9ea
MJ <mj838@outlook.com>
2016-04-10 04:04:00 +0100

    Create intial unit tests for holiday booking

---
+++ StaffPortalTests/Controllers/CalendarApiControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StaffPortal.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace StaffPortal.Controllers.Tests
+{
+    [TestClass]
+    public class CalendarApiControllerTests
+    {
+        [TestMethod]
+        public async Task BookHoliday_returns_400_if_start_date_is_after_end_date()
+        {
+            var controller = new CalendarApiController();
+            var result =
+                await controller.BookHoliday(new Models.HolidayBooking
+                {
+                    Start = new DateTime(2016, 1, 2),
+                    End = new DateTime(2016, 1, 1)
+                });
+
+            Assert.IsInstanceOfType(result, typeof(BadRequestErrorMessageResult));      
+        }
+
+        [TestMethod]
+        public async Task BookHoliday_returns_error_message_string_if_start_date_is_after_end_date()
+        {
+            var controller = new CalendarApiController();
+            var result =
+                await controller.BookHoliday(new Models.HolidayBooking
+                {
+                    Start = new DateTime(2016, 1, 2),
+                    End = new DateTime(2016, 1, 1)
+                });
+
+            Assert.AreEqual("Start date must be before end date.", ((BadRequestErrorMessageResult)result).Message);
+        }
+    }
+}
---
+++ StaffPortalTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StaffPortalTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StaffPortalTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c123e4c8-9229-4da5-802f-411a5a5e3192")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
